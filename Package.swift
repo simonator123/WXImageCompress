dfa7663bd521b3f60243e9c60bf2edea7a98c18d
Jared Simon <jared.d.simon@gmail.com>
2020-09-23 18:39:16 -0500

    Adding package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "Kingfisher",
+    platforms: [.iOS(.v10)]
+    products: [
+        .library(name: "WXImageCompress", targets: ["WXImageCompress"])
+   ],
+    targets: [
+        .target(
+            name: "WXImageCompress",
+            path: "Pod/Classes"
+        )
+    ]
+)
