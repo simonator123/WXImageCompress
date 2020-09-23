f8476272d9af516d6a8d78be7f86b3f03a97ef3b
Jared Simon <jared.d.simon@gmail.com>
2020-09-23 18:46:10 -0500

    Adding comma

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Kingfisher",
-    platforms: [.iOS(.v10)]
+    platforms: [.iOS(.v10)],
     products: [
         .library(name: "WXImageCompress", targets: ["WXImageCompress"])
    ],
